d38c49082b753aadfb1513b7ba7e736a503ae947
krishnachittodia <krishna.chittodia@successive.tech>
2020-09-12 00:29:04 +0530

    added react router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
+import NavBar from './components/Navbar';
+import { Home, Profile, SignUp, Login } from './components/screens'
+import './App.css';
 
 function App() {
   return (
-   <h1>Hello</h1>
+    <BrowserRouter>
+      <NavBar />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/screens/index.js
@@ -0,0 +1,11 @@
+import Home from './Home';
+import Login from './Login';
+import SignUp from './SignUp';
+import Profile from './Profile';
+
+export {
+  Login,
+  SignUp,
+  Home,
+  Profile,
+}
