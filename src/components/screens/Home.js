212cf2a2b750aef4169b77b27a3ea9f2dda15eb8
krishnachittodia <krishna.chittodia@successive.tech>
2020-09-12 16:27:45 +0530

    created profile and home page

--- src/components/screens/Home.js
+++ src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <div className="home">
+      <div className="card home-card">
+        <h5>Krishna</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing picture</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Krishna</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing picture</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Krishna</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1487088678257-3a541e6e3922?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+        </div>
+        <div className="card-content">
+          <i class="material-icons" style={{color: "red"}}>favorite</i>
+          <h6>title</h6>
+          <p>this is amazing picture</p>
+          <input type="text" placeholder="add comment" />
+        </div>
+      </div>
+    </div>
   )
 }
 
