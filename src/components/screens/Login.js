aaa263c8c452d9950ee896b9c5ac07b25f0c153f
krishnachittodia <krishna.chittodia@successive.tech>
2020-09-12 10:49:31 +0530

    added login and signup pages

--- src/components/screens/Login.js
+++ src/components/screens/Login.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <h1>Login</h1>
+    <div className="my-card">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input
+          type="text"
+          placeholder="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+        />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">Login</button>
+      </div>
+      <h5>
+          <Link to="/signup">Don't have an account?</Link>
+        </h5>
+    </div>
   )
 }
 
--- src/components/screens/SignUp.js
+++ src/components/screens/SignUp.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
   return (
-    <h1>SignUp</h1>
+    <div className="my-card">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input
+          type="text"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          placeholder="email"
+        />
+        <input
+          type="text"
+          placeholder="password"
+        />
+        <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">SignUp</button>
+        <h5>
+          <Link to="/login">Already have an account?</Link>
+        </h5>
+      </div>
+    </div>
   )
 }
 
