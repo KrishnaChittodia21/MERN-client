a7399b1aa34e9d22365afe4a0a8257cdd4e2e3f8
krishnachittodia <krishna.chittodia@successive.tech>
2020-09-12 17:14:15 +0530

    added create post page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import NavBar from './components/Navbar';
-import { Home, Profile, SignUp, Login } from './components/screens'
+import { Home, Profile, SignUp, Login, CreatePost } from './components/screens'
 import './App.css';
 
 function App() {
@@ -19,6 +19,9 @@ function App() {
       </Route>
       <Route path="/profile">
         <Profile />
+      </Route>
+      <Route path="/create">
+        <CreatePost />
       </Route>
     </BrowserRouter>
   );
