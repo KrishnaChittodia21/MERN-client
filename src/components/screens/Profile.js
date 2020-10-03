d67fd839c7120627a47a75640db3311c3a412495
KrishnaChittodia21 <krishnachittodia.10@gmail.com>
2020-10-04 00:51:02 +0530

    added update pic functionality

--- src/components/screens/Profile.js
+++ src/components/screens/Profile.js
@@ -4,6 +4,8 @@ import { UserContext } from '../../App';
 const Profile = () => {
   const [myPics, setPics] = useState([]);
   const { state, dispatch } = useContext(UserContext);
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState("");
   useEffect(() => {
     fetch('/mypost', {
       headers: {
@@ -15,26 +17,69 @@ const Profile = () => {
       setPics(result.mypost);
     })
   }, [])
+
+  useEffect(() => {
+    if(image) {
+      setImage(image)
+    }
+  }, [image])
+
+  const updateProfilePic = (file) => {
+    setImage(file);
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "insta-clone");
+    data.append("cloud_name", "kannu21");
+    fetch("https://api.cloudinary.com/v1_1/kannu21/image/upload", {
+      method: "post",
+      body:  data
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      setUrl(data.url);
+      localStorage.setItem('user', JSON.stringify({...state, pic: data.url }))
+      dispatch({ type: 'UPDATEPIC', payload: data.url})
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+  }
+
   return (
     <div style={{maxWidth:"550px", margin: "0px auto"}}>
-      <div style={{
-        display: "flex",
-        justifyContent: "space-around",
+      <div style={{ 
         margin: "18px 0px",
-        borderBottom: "1px solid grey"
-      }}>
-        <div>
-          <img alt="" style={{width:"160px", height:"160px", borderRadius: "80px"}}
-            src={state?state.pic: ""}
-          />
-        </div>
-        <div>
-          <h4>{state? state.name : 'loading...'}</h4>
-          <h5>{state? state.email : 'loading...'}</h5>
-          <div style={{display: "flex", justifyContent: "space-between", width: "108%"}}>
-            <h6>{myPics.length} posts</h6>
-            <h6>{state?state.followers.length:0} followers</h6>
-            <h6>{state?state.following.length:0} following</h6>
+        borderBottom: "1px solid grey"}}>
+        <div style={{
+          display: "flex",
+          justifyContent: "space-around",
+        }}>
+          <div>
+            <img alt="" style={{width:"160px", height:"160px", borderRadius: "80px"}}
+              src={state?state.pic: ""}
+            />
+          </div>
+          <div>
+            <h4>{state? state.name : 'loading...'}</h4>
+            <h5>{state? state.email : 'loading...'}</h5>
+            <div style={{display: "flex", justifyContent: "space-between", width: "108%"}}>
+              <h6>{myPics.length} posts</h6>
+              <h6>{state && state.followers ?state.followers.length:0} followers</h6>
+              <h6>{state && state.following ?state.following.length:0} following</h6>
+            </div>
+          </div>
+        </div> 
+        <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+          style={{ margin: '10px 0px 10px 52px'}}
+          onClick={() => updateProfilePic()}
+        >Update pic</button>
+        <div className="file-field input-field" style={{ margin: '10px'}}>
+          <div className="btn #64b5f6 blue darken-1">
+            <span>Update pic</span>
+            <input type="file" onChange={(e) => updateProfilePic(e.target.files[0])} />
+          </div>
+          <div className="file-path-wrapper">
+            <input className="file-path validate" type="text" />
           </div>
         </div>
       </div>
